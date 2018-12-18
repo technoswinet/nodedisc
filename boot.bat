5838c999b417f24df914b46e2db7d1ed481d44a5
technoswinet <45797582+technoswinet@users.noreply.github.com>
2018-12-18 11:51:04 +0100

    Add files via upload

--- boot.bat
+++ boot.bat
@@ -8,7 +8,24 @@ set author = ""
 for /f %%i in ('ini.bat config author') do set author=%%i
 set github = ""
 for /f %%i in ('ini.bat config github') do set github=%%i
+set botDir = ""
+for /f %%i in ('ini.bat config bot') do set botDir=%%i
 
+echo Batch is setup by %author% 
+echo --------------------------------------------------------
 echo %github%
+echo --------------------------------------------------------
+echo bot: %token%
+echo --------------------------------------------------------
 
+echo ctrl+c to exit
+
+
+node %botDir%
+:safe
+goto safe
+
+
+
+echo something bad has happened.
 pause
